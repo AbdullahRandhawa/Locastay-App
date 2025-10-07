256d30e5f107944094aa4208d14bddd679ed037d
web-dev <abrandhawa25@gmail.com>
2025-10-07 18:31:18 +0500

    Enabling dynamic port

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.use((err, req, res, next) => {
 
 
 // Listining Port--------------------------
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);

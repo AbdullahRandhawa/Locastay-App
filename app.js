ab7ec5e929a49f12c808d484c499ce8cf0d0125c
web-dev <abrandhawa25@gmail.com>
2026-01-25 21:01:08 +0500

    Port problem

--- app.js
+++ app.js
@@ -148,7 +148,6 @@ app.use((err, req, res, next) => {
 
 // Listining Port--------------------------
 const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
